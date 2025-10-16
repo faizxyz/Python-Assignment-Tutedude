05e5d7f4c126326940e3029ce7484bcb7e675bbb
faizxyz <faizsayedxyz@gmail.com>
2025-10-16 16:41:58 +0530

    initiak commit

---
+++ Task 1.py
@@ -0,0 +1,16 @@
+#Task 1
+
+num1 = float(input('Enter the first number:'))
+num2 = float(input('Enter the second number:'))
+print('Addition =', num1 + num2)
+print('Subtraction =', num1 - num2)
+print('Multiplication: ',num1 * num2)
+print('Division =', num1 / num2)
+
+git config --global user.name "faizxyz"
+git config --global user.email "faizsayedxyz@gmail.com"
+
+git init
+
+git pull origin main
+git status
