3ecf19ebb8190ab2003b8850697d1834289952c6
faizxyz <123842158+faizxyz@users.noreply.github.com>
2025-10-16 16:57:58 +0530

    Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -7,16 +7,3 @@ print('Addition =', num1 + num2)
 print('Subtraction =', num1 - num2)
 print('Multiplication: ',num1 * num2)
 print('Division =', num1 / num2)
-
-git config --global user.name "faizxyz"
-git config --global user.email "faizsayedxyz@gmail.com"
-
-git init
-
-git pull origin main
-git status
-
-git add .
-git status
-git commit -m "Commit Message"
-git push
